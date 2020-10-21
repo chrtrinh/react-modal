bc2d0cfc78110f51cf5eecc0eb389529d0b8d8e9
Christopher Trinh <Chris.trinh@live.com>
2020-10-21 10:31:01 -0400

    Built out React Modal using styled components

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './App.css';
+import styled from 'styled-components';
+import Modal from './components/Modal';
+import { GlobalStyle } from './globalStyles';
+
+const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+`;
+
+const Button = styled.button`
+	min-width: 100px;
+	padding: 16px 32px;
+	border-radius: 4px;
+	border: none;
+	background: #141414;
+	color: #fff;
+	font-size: 24px;
+	cursor: pointer;
+`;
+
+function App() {
+	const [showModal, setShowModal] = useState(false);
+
+	const openModal = () => {
+		setShowModal((prev) => !prev);
+	};
+
+	return (
+		<div className="app">
+			<Container>
+				<Button onClick={openModal}>I'm a Modal</Button>
+				<Modal showModal={showModal} setShowModal={setShowModal} />
+				<GlobalStyle />
+			</Container>
+		</div>
+	);
+}
+
+export default App;
